ed545e7098cd20ed8be185c25fd69e56bc840f7f
Damian9696 <damian.ogorek.1996@gmail.com>
2021-07-13 22:06:13 +0200

    Filled grid with data mapped into grid item;

--- lib/categories_screen.dart
+++ lib/categories_screen.dart
@@ -1,12 +1,15 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_complete_guide/category_item.dart';
+import 'package:flutter_complete_guide/models/dummy_data.dart';
 
 class CategoriesScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return GridView(
-        children: [
-
-        ],
+        children: DUMMY_CATEGORIES
+            .map((categoryData) =>
+                CategoryItem(categoryData.title, categoryData.color))
+            .toList(),
         gridDelegate: SliverGridDelegateWithMaxCrossAxisExtent(
           maxCrossAxisExtent: 200,
           childAspectRatio: 1.5,
