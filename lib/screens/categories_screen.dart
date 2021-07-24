2842fc6a50cd39640a9bab11792df79ebbf775a2
Damian <damian.ogorek.1996@gmail.com>
2021-07-24 23:23:41 +0200

    Updated packages;

--- lib/screens/categories_screen.dart
+++ lib/screens/categories_screen.dart
@@ -1,5 +1,5 @@
 import 'package:flutter/material.dart';
-import 'package:flutter_complete_guide/category_item.dart';
+import 'package:flutter_complete_guide/widgets/category_item.dart';
 import 'package:flutter_complete_guide/models/dummy_data.dart';
 
 class CategoriesScreen extends StatelessWidget {
